be7a0d6d8fb9c69c455b41790e4c8125af556000
ben <loudtiger@gmail.com>
2014-03-15 14:03:49 -0500

    Set up heroku

--- LameWorkExcuses.py
+++ LameWorkExcuses.py
@@ -1,21 +1,20 @@
-from bottle import route, run, template
 from flask import Flask, g, jsonify, redirect, render_template, request, url_for
 
 import random
 
-app = Flask(__name__)
+excuses = []
 
-@app.route('/', methods=['GET'])
-def index():
-	excuses = []
+def init_app():
 	with open('excuses.txt') as f:
 		for line in f:
 			excuses.append(line)
-	
-	chosenExcuse = random.choice(excuses)
-	
-	return render_template('index.html', theExcuse=chosenExcuse)
 
-if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0', port=5002)
+	app = Flask(__name__)
+	return app
+
+app = init_app()
+
+@app.route('/', methods=['GET'])
+def index():
+	chosenExcuse = random.choice(excuses)
+	return render_template('index.html', theExcuse=chosenExcuse)
